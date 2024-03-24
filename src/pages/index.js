e344ac6de9f756e20336c1b697dc1c30c6621a26
MohitAlphonsus <mohitdalphonso@gmail.com>
2024-03-24 11:26:59 +0530

    all the required pages throughout the application

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import Home from './Home';
+import Details from './Details';
+import Explore from './Explore';
+import SearchResult from './SearchResult';
+import PageNotFound from './PageNotFound';
+
+export { Home, Details, Explore, SearchResult, PageNotFound };
