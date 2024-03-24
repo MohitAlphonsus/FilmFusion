0e1fbf7214b75f8aff5e00c1569076b8b5e649e4
MohitAlphonsus <mohitdalphonso@gmail.com>
2024-03-24 11:27:27 +0530

    react-components

---
+++ src/components/Hero.jsx
@@ -0,0 +1,64 @@
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import useFetch from '../hooks/useFetch';
+import { LazyLoadImg } from '../components';
+import { Container } from '../UI';
+
+import './Hero.scss';
+
+function Hero() {
+	const [backgroundCover, setBackgroundCover] = useState('');
+	const [query, setQuery] = useState('');
+	const navigate = useNavigate();
+	const { url } = useSelector(state => state.home);
+
+	const { data, loading } = useFetch('/movie/upcoming');
+
+	useEffect(
+		function () {
+			const backdrop =
+				url.backdrop +
+				data?.results[Math.floor(Math.random() * 20)]?.backdrop_path;
+			setBackgroundCover(backdrop);
+		},
+		[data, url.backdrop],
+	);
+
+	function handleSearch(e) {
+		if (e.key === 'Enter' && query.length > 0) {
+			navigate(`/search/${query}`);
+		}
+	}
+
+	return (
+		<div className="hero">
+			{!loading && (
+				<div className="hero__backdrop">
+					<LazyLoadImg src={backgroundCover} />
+				</div>
+			)}
+			<Container>
+				<div className="hero__content">
+					<span className="hero__heading">
+						Dive Into a World of Cinematic Wonder
+					</span>
+					<span className="hero__subheading">
+						Explore Thousands of Movies & TV shows at Your Fingertips
+					</span>
+					<div className="hero__search-bar">
+						<input
+							type="text"
+							placeholder="Search for movie of tv show..."
+							onChange={e => setQuery(e.target.value)}
+							onKeyUp={handleSearch}
+						/>
+						<button>Explore</button>
+					</div>
+				</div>
+			</Container>
+		</div>
+	);
+}
+
+export default Hero;
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import Header from './Header';
+import Hero from './Hero';
+import Footer from './Footer';
+import LazyLoadImg from './LazyLoadImg';
+
+export { Header, Hero, LazyLoadImg, Footer };
