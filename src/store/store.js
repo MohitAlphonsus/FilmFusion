86e3061554294787f53a4cef78ee9d5a5caf05e1
MohitAlphonsus <mohitdalphonso@gmail.com>
2024-03-24 11:20:27 +0530

    redux store

--- src/store/store.js
+++ src/store/store.js
@@ -1,3 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
+import homeSlice from './homeSlice';
 
-export const store = configureStore({});
+export const store = configureStore({
+	reducer: {
+		home: homeSlice,
+	},
+});
