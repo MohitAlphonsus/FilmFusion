678b4f99d2e33367688f3f4d74aed8c78bd61238
MohitAlphonsus <mohitdalphonso@gmail.com>
2024-03-30 08:35:39 +0530

    circlerating component

--- src/components/CircleRating.jsx
+++ src/components/CircleRating.jsx
@@ -1,5 +1,11 @@
 function CircleRating() {
-	return <div>CircleRating</div>;
+	return (
+		<div className="circle">
+			<div className="circle__inner">
+				<span className="circle__rating">8.2</span>
+			</div>
+		</div>
+	);
 }
 
 export default CircleRating;
