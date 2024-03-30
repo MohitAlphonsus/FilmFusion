7b431d64a15ff787a6a50a2d21ced2533969c45c
MohitAlphonsus <mohitdalphonso@gmail.com>
2024-03-30 13:06:07 +0530

    popular tv and movies rendered

---
+++ src/components/Popular.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import { CarousalSection } from '../Layouts';
+import { useFetch } from '../hooks';
+import { Carousal } from '../components';
+
+function Popular() {
+	const [endPoint, setEndPoint] = useState('movie');
+
+	const { data, loading } = useFetch(`/${endPoint}/popular`);
+
+	function tabChangHandler(tab) {
+		setEndPoint(tab === 'Movies' ? 'movie' : 'tv');
+	}
+
+	return (
+		<>
+			<CarousalSection
+				title="What's Popular"
+				tabData={['Movies', 'TV Shows']}
+				onTabChange={tabChangHandler}
+			/>
+			<Carousal data={data?.results} loading={loading} />
+		</>
+	);
+}
+
+export default Popular;
