b1e33f23b2ce9e7ed2e230b4793692da6e67a7d2
MohitAlphonsus <mohitdalphonso@gmail.com>
2024-03-30 08:35:54 +0530

    imports

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import Hero from './Hero';
 import Trending from './Trending';
 import Carousal from './Carousal';
+import CircleRating from './CircleRating';
 
-export { Hero, Trending, Carousal };
+export { Hero, Trending, Carousal, CircleRating };
