b82277c83b8f6c63f00f506705cde4b25fdbeda2
MohitAlphonsus <mohitdalphonso@gmail.com>
2024-04-02 10:34:33 +0530

    imports

--- src/components/index.js
+++ src/components/index.js
@@ -5,5 +5,17 @@ import TopRated from './TopRated';
 import Carousal from './Carousal';
 import CircleRating from './CircleRating';
 import Genres from './Genres';
+import DetailsBanner from './DetailsBanner';
+import PlayButton from './PlayButton';
 
-export { Hero, Trending, Popular, Carousal, TopRated, CircleRating, Genres };
+export {
+	Hero,
+	Trending,
+	Popular,
+	Carousal,
+	TopRated,
+	CircleRating,
+	Genres,
+	DetailsBanner,
+	PlayButton,
+};
